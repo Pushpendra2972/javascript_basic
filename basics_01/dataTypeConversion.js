0de59ccad36e2f5aed8fa5fb3f3a4be9ab858816
Pushpendra Kumar <pkverma2972@gmail.com>
2025-12-22 13:00:18 +0000

    Conversion of data types

---
+++ basics_01/dataTypeConversion.js
@@ -0,0 +1,27 @@
+let score = "78abc"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+let valuInBool = Boolean(score)
+
+console.log(typeof valueInNumber)
+console.log(typeof valueInBool)
+console.log(valueInNumber)
+
+let num = 1
+
+let num_toBool = Boolean(num)
+
+
+console.log(typeof  num_toBool)
+console.log( num_toBool)
+
+
+// "78" => 78
+// "78abc" => NaN
+// true => 1; false => 0
+// "" => false
+// "Pushpendra" => true
+
+
