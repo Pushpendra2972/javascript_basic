cbffe388088844e86a51cece4c8fe5bec74f493e
Pushpendra Kumar <pkverma2972@gmail.com>
2025-12-22 18:31:05 +0000

    data type summary

---
+++ basics_01/dataTypesSummary.js
@@ -0,0 +1,27 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = symbol('123')
+
+const bigNumber = 12345679876345n
+
+// Reference (non primitive)
+
+// Array, Object, Function
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Pushpendra Kumar",
+    age: 21,
+}
+
+const myFunction = function (){
+    console.log("hellow world")
+}
