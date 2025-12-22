f747eac9ac6a048f6a72411657e5ac7da3ea2e89
Pushpendra Kumar <pkverma2972@gmail.com>
2025-12-22 12:18:19 +0000

    datatypes

---
+++ basics_01/datatypes.js
@@ -0,0 +1,29 @@
+"use strict" ;  // treat all js code as newer version
+
+// alert("hellow")   // we are using node js not browser
+
+// console.log(3+3)
+// console.log("Pushpendra")
+
+let name = "Pushpendra"
+let age = 21
+let flag = true
+
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// bool =>true/false
+// null =>stand alone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "pkv")
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof flag)
+console.log(typeof null)
+console.log(typeof undefined)
