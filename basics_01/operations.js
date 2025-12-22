65d65808b8784fddc86fecefc8f0db115d50c5ac
Pushpendra Kumar <pkverma2972@gmail.com>
2025-12-22 14:26:50 +0000

    operations learnt

---
+++ basics_01/operations.js
@@ -0,0 +1,41 @@
+let value = 3
+let negValue = -value
+
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2%2)
+// console.log(2*2)
+
+let str1 = "Hellow"
+let str2 = "World"
+let str3 = str1 + str2
+
+console.log(str3)            // HellowWorld
+
+console.log("1" + 2)          // 12
+console.log( 1 + "2")         // 12
+console.log( 1 + 3 + "2")     // 42
+console.log( "1" + 3 + 2)     // 132
+console.log( 1 + 2 + "3" + 4 + 5)   //3345
+
+// console.log( 1  + 2 - "5" + 5 + 8)   // 11
+// console.log( 1 - "5")         // -4
+// console.log( "1" - 5)         // -4
+// console.log( 1 * "5")         // 5
+// console.log( "1" * 5)         // 5
+
+console.log(+true)   // 1
+console.log(+"")     // 0
+
+let num1, num2, num3 
+
+num1 = num2 = 4
+
+num1++, num2--;
+
+console.log(num1)     // 5
+console.log(++num1)   // 6
+console.log(num2++)    // 3
+console.log(num2)      // 4
