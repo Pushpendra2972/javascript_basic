3c9f747f9b66ab5a35daf335427b2575d28a158b
Pushpendra Kumar <pkverma2972@gmail.com>
2025-12-22 14:50:45 +0000

    comparison of datatypes

---
+++ basics_01/comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1)    // boolean ans true/false
+// console.log(2 >= 1)
+// console.log(2 != 1)
+// console.log(2 < 1)
+
+console.log("0" > 1)     // false
+console.log("2" > 1)     // true
+
+console.log(null == 0)   // false
+
+// in comparison converts null to a number 0 
+
+console.log(null > 0)     // false
+console.log(null >= 0)    // true
+
+// ===
+
+console.log("2" == 2)    // true
+console.log("2" === 2)   // false  // no conversion
