5227b2dd624dba7ae05c51025228f5ed78ca0be1
Pushpendra Kumar <pkverma2972@gmail.com>
2025-12-25 19:51:52 +0000

    arrays

--- basics_02/arrays.js
+++ basics_02/arrays.js
@@ -2,33 +2,33 @@
 
 // console.log(myArray)
 
-// const myArray = new Array(1, 2, 3, 4, 5, 6)
+const myArray = new Array(1, 2, 3, 4, 5, 6)
 
-// console.log(myArray)     // [ 1, 2, 3, 4, 5, 6 ]
-
-
-// console.log(myArray[0])       // 1
-// console.log(myArray.length)   // 6
+console.log(myArray)     // [ 1, 2, 3, 4, 5, 6 ]
 
 
-// myArray.push(7) 
-// myArray.push(8)
-// myArray.pop()
-
-// console.log(myArray)         // [ 1, 2, 3, 4, 5, 6, 7 ]
+console.log(myArray[0])       // 1
+console.log(myArray.length)   // 6
 
 
+myArray.push(7) 
+myArray.push(8)
+myArray.pop()
 
-// console.log(myArray.unshift(10))      // 8 updated length and adding element from the front of the array
-// console.log(myArray.unshift(11))      // 9
-
-//  console.log(myArray)           // [11, 10, 1, 2, 3, 4, 5, 6, 7 ]
+console.log(myArray)         // [ 1, 2, 3, 4, 5, 6, 7 ]
 
 
 
-//  console.log(myArray.shift())    // 11 taking out front element
+console.log(myArray.unshift(10))      // 8 updated length and adding element from the front of the array
+console.log(myArray.unshift(11))      // 9
 
-//  console.log(myArray)         // [ 10, 1, 2, 3, 4, 5, 6, 7 ]
+ console.log(myArray)           // [11, 10, 1, 2, 3, 4, 5, 6, 7 ]
+
+
+
+ console.log(myArray.shift())    // 11 taking out front element
+
+ console.log(myArray)         // [ 10, 1, 2, 3, 4, 5, 6, 7 ]
 
 
 
