25d31c7e17ee38be93de5dbf300649daf6ce6722
Pushpendra Kumar <pkverma2972@gmail.com>
2026-01-02 07:46:43 +0000

    functions

---
+++ basics_03/functions.js
@@ -0,0 +1,61 @@
+// no argument passed
+
+function myName(){
+    console.log("hello , welcome to function lecture")
+}
+
+myName()
+
+
+
+// with argument
+
+function print(num1, num2){
+    console.log(num1 + num2)
+}
+
+print(1, 2)
+
+
+
+
+// return type function
+
+function findMaxValue(num1 , num2){
+    return Math.max(num1, num2)
+}
+
+console.log(findMaxValue(5, 7))
+
+const value = findMaxValue(5, 10)
+console.log(value)
+
+
+
+// when no argument is passed 
+
+
+function yourName(name){
+    return `my name is ${name}`
+}
+
+console.log(yourName())               // my name is undefined
+console.log(yourName("Pushpa"))
+
+
+
+// if / else
+
+
+function loggedIn(name){
+    if(name){
+        console.log( `${name} is logged in`)
+    }
+    else{
+        console.log(`please give me person name who is logged in`)
+    }
+
+}
+
+console.log(loggedIn())
+console.log(loggedIn("ram"))
